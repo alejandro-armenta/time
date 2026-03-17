5ab63fde39f1d2fb2b8b1de4d78dae5e464423a9
Alejandro <armentaayalaalejandro@gmail.com>
2026-03-16 22:38:22 -0600

    trained multiple arima

--- sarima.py
+++ sarima.py
@@ -6,7 +6,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.arima_process import ArmaProcess
 from statsmodels.graphics.gofplots import qqplot
 from statsmodels.tsa.stattools import adfuller
-from tqdm import tqdm_notebook
+from tqdm import tqdm
 from itertools import product
 from typing import Union
 
@@ -44,4 +44,64 @@ fig.autofmt_xdate()
 plt.tight_layout()
 plt.savefig('decomposition.png')
 
+print(adfuller(df['Passengers']))
+
+df_diff = np.diff(df['Passengers'], n=2)
+
+print(adfuller(df_diff))
+
+
+p = range(0,13,1)
+q = range(0,13,1)
+
+P = [0]
+Q = [0]
+
+d = 2
+D = 0
+s = 12
+
+#13*13
+ARIMA_order_list = list(product(p,q,P,Q))
+
+def optimize_SARIMA(endog: Union[pd.Series, list], order_list: list, d: int, D:int, s:int) -> pd.DataFrame:
+
+    results = []
+
+    a = tqdm(order_list)
+
+    for p, q, P, Q in a:
+
+        try:
+            model = SARIMAX(
+                endog, 
+                order=(p, d, q),
+                seasonal_order=(P, D, Q, s),
+                simple_differencing=False
+                ).fit(disp=False)
+            
+        except:
+            continue
+
+        aic = model.aic
+
+        results.append([(p, d, q, P, D, Q, s), aic])
+    
+    result_df = pd.DataFrame(results)
+
+    result_df.columns = ['(p,d,q,P,D,Q,s)', 'AIC']
+
+    result_df.sort_values(by='AIC', ascending=True, inplace=True)
+
+    result_df.reset_index(drop=True, inplace=True)
+
+    return result_df
+
+train = df['Passengers'][:-12]
+
+#print(df['Passengers'])
+
+result_df = optimize_SARIMA(train, ARIMA_order_list, d, D, s)
+
+result_df.to_csv('ARIMA_result.csv', index=True)
 
